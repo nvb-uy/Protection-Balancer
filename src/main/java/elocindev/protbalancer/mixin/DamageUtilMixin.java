f6cd28ab23a80772f050eb07fac31081bd89b831
nvb-uy <almhak@proton.me>
2024-03-17 12:56:51 -0300

    fix(zenith/apothic attributes compat)

--- src/main/java/elocindev/protbalancer/mixin/DamageUtilMixin.java
+++ src/main/java/elocindev/protbalancer/mixin/DamageUtilMixin.java
@@ -2,8 +2,9 @@ package elocindev.protbalancer.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 
 import elocindev.protbalancer.ProtBalancer;
 import elocindev.protbalancer.math.FormulaParser;
@@ -12,8 +13,8 @@ import net.minecraft.entity.DamageUtil;
 @Mixin(DamageUtil.class)
 public class DamageUtilMixin {
 
-    @Inject(method = "getDamageLeft", at = @At("HEAD"), cancellable = true)
-    private static void getDamageLeft(float damage, float armor, float armorToughness, CallbackInfoReturnable<Float> ci) {
+    @ModifyReturnValue(method = "getDamageLeft", at = @At("RETURN"))
+    private static float getDamageLeft(float original, float damage, float armor, float armorToughness) {
         String formula = ProtBalancer.CONFIG.armor_formula;
 
         if (ProtBalancer.CONFIG.enable_armor_formula && !formula.isEmpty()) {
@@ -21,19 +22,23 @@ public class DamageUtilMixin {
             formula = formula.replace("ARMOR", String.valueOf(armor));
             formula = formula.replace("ARMOR_TOUGHNESS", String.valueOf(armorToughness));
 
-            ci.setReturnValue(damage * (1.0F - (float) FormulaParser.evaluateFormula(formula)));
+            return (float) (damage * (1.0F - FormulaParser.evaluateFormula(formula)));
         }
+
+        return original;
     }
   
-    @Inject(method = "getInflictedDamage", at = @At("HEAD"), cancellable = true)
-    private static void getInflictedDamage(float damageDealt, float protection, CallbackInfoReturnable<Float> ci) {
+    @ModifyReturnValue(method = "getInflictedDamage", at = @At("RETURN"))
+    private static float getInflictedDamage(float original, float damage, float protection) {
         String formula = ProtBalancer.CONFIG.protection_formula;
 
         if (ProtBalancer.CONFIG.enable_protection_formula && !formula.isEmpty()) {
-            formula = formula.replace("DAMAGE", String.valueOf(damageDealt));
+            formula = formula.replace("DAMAGE", String.valueOf(damage));
             formula = formula.replace("PROTECTION", String.valueOf(protection));
 
-            ci.setReturnValue(damageDealt * (1.0F - (float) FormulaParser.evaluateFormula(formula)));
+            return (float) (damage * (1.0F - FormulaParser.evaluateFormula(formula)));
         }
+        
+        return original;
     }
 }
